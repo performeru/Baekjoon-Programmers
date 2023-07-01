9e8549dc9ca2f8d61ce6ce790f463ad49d1d57ba
performeru <130340605+performeru@users.noreply.github.com>
2023-07-01 21:42:54 +0900

    [unrated] Title: 문자열 반복해서 출력하기, Time: 2.02 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+
+int main() 
+{
+    std::string str;
+    int n;
+    
+    std::cin >> str >>n;
+    
+    for(int i=0; i<n; i++)
+    {
+     std::cout << str;
+    }
+    
+}
