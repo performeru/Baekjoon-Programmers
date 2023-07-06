aea7fc32c888413f04522f34f5a6962fc62687e0
performeru <130340605+performeru@users.noreply.github.com>
2023-07-06 20:26:43 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+    bool submitted[31]{ false }; 
+
+    for (int i = 0; i < 28; i++)
+    {
+        int number;
+        std::cin >> number;
+        submitted[number] = true; 
+    }
+    
+    int firstMissing = -1; 
+    int secondMissing = -1;
+
+    for (int i = 1; i <= 30; i++)
+    {
+        if (!submitted[i])
+        {
+            if (firstMissing == -1)
+            {
+                firstMissing = i;
+            }
+            else
+            {
+                secondMissing = i;
+                break;
+            }
+        }
+    }
+
+    std::cout << firstMissing << std::endl;
+    std::cout << secondMissing << std::endl;;
+
+    return 0;
+}
