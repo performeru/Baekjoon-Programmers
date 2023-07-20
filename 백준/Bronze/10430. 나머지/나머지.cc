83646f3e7785d63c885ac01c589b32342407a78a
performeru <130340605+performeru@users.noreply.github.com>
2023-07-20 16:56:52 +0900

    Update 나머지.cc

--- 백준/Bronze/10430. 나머지/나머지.cc
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -1,14 +1,45 @@
 #include <iostream>
 
+class Calculator
+{
+private:
+	int mA, mB, mC;
+
+public:
+
+	Calculator(int a, int b, int c) : mA(a), mB(b), mC(c) {}
+
+    int SumModuloC() const
+    {
+        return (mA + mB) % mC;
+    }
+
+    int SumModuloCIndividual() const
+    {
+        return ((mA % mC) + (mB % mC)) % mC;
+    }
+
+    int MultiplyModuloC() const
+    {
+        return (mA * mB) % mC;
+    }
+
+    int MultiplyModuloCIndividual() const
+    {
+        return ((mA % mC) * (mB % mC)) % mC;
+    }
+};
+
 int main()
 {
-    int a,b,c;
-    std::cin >> a >> b >> c;
-    std::cout << (a+b)%c << std::endl; 
-    std::cout << ((a%c)+(b%c))%c << std::endl;
-    std::cout << (a*b)%c << std::endl;
-    std::cout << ((a%c)*(b%c))%c << std::endl;
-    
-    return 0;
-    
+	int a{}, b{}, c{};
+
+	std::cin >> a >> b >> c;
+
+    Calculator calculator(a, b, c);
+
+    std::cout << calculator.SumModuloC() << std::endl;
+    std::cout << calculator.SumModuloCIndividual() << std::endl;
+    std::cout << calculator.MultiplyModuloC() << std::endl;
+    std::cout << calculator.MultiplyModuloCIndividual() << std::endl;
 }
