9e7fb78418a58822cb4a1ce3f6f64639549d6572
performeru <130340605+performeru@users.noreply.github.com>
2023-10-07 10:20:32 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    
+    int A, B, T;
+	std::cin >> T;
+
+	for (int i = 1; i <= T; i++)
+	{
+		std::cin >> A >> B;
+		std::cout << A+B << std::endl;			
+	}
+
+
+}
