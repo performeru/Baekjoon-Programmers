eed4bf9ba8d32938fa8f266f7047dde211010ead
performeru <130340605+performeru@users.noreply.github.com>
2023-10-19 18:22:23 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        string str;
+        cin >> str;
+        cout << str[0] << str[str.length()-1] << endl;
+    }
+}
