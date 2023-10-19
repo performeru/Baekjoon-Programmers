217c04034631716c04fd71f223e973629fdd7cca
performeru <130340605+performeru@users.noreply.github.com>
2023-10-19 18:40:28 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    string str;
+    cin >> str;
+    
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++)
+    {
+       sum += str[i] - '0';
+    }
+    
+    cout << sum;
+}
+
+
+
+
+
