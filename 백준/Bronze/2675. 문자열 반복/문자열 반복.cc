1d6648a1bc220f695982529d57b83b80e265539e
performeru <130340605+performeru@users.noreply.github.com>
2023-11-03 20:54:41 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    
+    cin >> T; 
+
+    while (T--)
+    {
+        int R;
+        string S;
+        
+        cin >> R >> S; 
+
+        for (char c : S) 
+        {
+            for (int i = 0; i < R; ++i) 
+            {
+                cout << c;
+            }
+        }
+        cout << endl;
+    }
+}
