1b0a245e0c1848e7d95d7788f8c5212e83e8e5d9
performeru <130340605+performeru@users.noreply.github.com>
2023-12-05 18:45:46 +0900

    [Bronze I] Title: 단어 공부, Time: 40 ms, Memory: 2876 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+int main() 
+{
+    char word[1000001];
+    int count[26] = {0};
+
+    cin.getline(word, sizeof(word)); 
+
+    int len = strlen(word);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (isalpha(word[i])) 
+        {
+            count[toupper(word[i]) - 'A']++;
+        }
+    }
+
+    int maxCount = 0;
+    char result = '?';
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (count[i] > maxCount) 
+        {
+            maxCount = count[i];
+            result = 'A' + i;
+        }
+        else if (count[i] == maxCount)
+        {
+            result = '?';
+        }
+    }
+    cout << result << endl;
+
+}
