a786609b5de1ad99eab2c4896a4dc427621206a2
performeru <130340605+performeru@users.noreply.github.com>
2024-01-09 22:36:55 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) 
+{
+    string answer = "";
+    
+    for(char c : my_string)
+    {
+        answer += string(n, c);
+    }
+    return answer;
+}
