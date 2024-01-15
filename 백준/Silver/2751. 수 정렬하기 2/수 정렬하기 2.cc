2bb32c6e660bcadca1120fd8320904f987a49056
performeru <130340605+performeru@users.noreply.github.com>
2024-01-15 23:33:29 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 276 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> num(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> num[i];
+    }
+    
+    sort(num.begin(), num.end());
+    
+    for(int i = 0; i < n; i++)
+    {
+        cout << num[i] << "\n";
+    }
+   
+}
