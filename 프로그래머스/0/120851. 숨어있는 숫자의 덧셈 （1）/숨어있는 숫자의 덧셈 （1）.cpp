b8d7a9d09d608165b36d7c9164962f6d5211d793
performeru <130340605+performeru@users.noreply.github.com>
2024-01-18 22:21:39 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+int solution(string my_string) 
+{
+    int answer = 0;
+    
+    for(int i = 0; i < my_string.size(); i++)
+    {
+        if(isdigit(my_string[i]))
+        {
+            answer += my_string[i] - '0';
+        }
+    }
+    
+    return answer;
+}
