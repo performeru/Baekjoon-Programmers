2ab831250b6588e8212b30299fedc000dba5f253
performeru <130340605+performeru@users.noreply.github.com>
2024-02-08 15:24:55 +0900

    [level 0] Title: ad 제거하기, Time: 0.23 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr) 
+{
+    vector<string> answer;
+    
+    for(auto str : strArr)
+    {
+        if(str.find("ad") == string::npos)
+        {
+            answer.push_back(str);
+        }
+    }
+    
+    return answer;
+}
