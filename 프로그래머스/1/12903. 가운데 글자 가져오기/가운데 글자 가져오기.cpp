136c9c1eaaf69ee7c49a65ffdc8046b5c62a6c03
performeru <130340605+performeru@users.noreply.github.com>
2024-02-09 13:52:58 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+using namespace std;
+
+string solution(string s) 
+{
+    string answer = "";
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s.length() % 2 == 0)
+        {
+            answer = s.substr(s.length() / 2- 1 ,2);
+        }
+        else
+        {
+            answer = s.substr(s.length() / 2, 1);
+        }
+    }
+    return answer;
+}
