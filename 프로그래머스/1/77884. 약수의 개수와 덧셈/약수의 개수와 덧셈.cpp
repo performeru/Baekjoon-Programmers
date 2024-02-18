ada2d73d88a7e87e2403095898329ec5ec76d4f9
performeru <130340605+performeru@users.noreply.github.com>
2024-02-18 20:47:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int countDivisors(int num) 
+{
+    int count = 0;
+    
+    for (int i = 1; i <= num; ++i) 
+    {
+        if (num % i == 0) 
+        {
+            ++count;
+        }
+    }
+    return count;
+}
+
+int solution(int left, int right) 
+{
+    int answer = 0;
+    
+    for (int i = left; i <= right; ++i) 
+    {
+        int divisorCount = countDivisors(i);
+        if (divisorCount % 2 == 0) 
+        {
+            answer += i; 
+        } 
+        else
+        {
+            answer -= i; 
+        }
+    }
+    return answer;
+}
