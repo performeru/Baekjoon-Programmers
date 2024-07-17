d7307bf3b7bc402d169a78322b1522a2985b17bd
performeru <130340605+performeru@users.noreply.github.com>
2024-07-17 22:09:43 +0900

    Update 유기농 배추.cc

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -16,14 +16,16 @@ int main()
     int t;
     cin >> t;
     
-    while (t--) {
+    while (t--) 
+    {
         int m, n, k;
         cin >> m >> n >> k;
         
         vector<vector<int>> field(m, vector<int>(n, 0));
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) 
+        {
             int x, y;
             cin >> x >> y;
             field[x][y] = 1;
@@ -32,23 +34,29 @@ int main()
         int count = 0;
         queue<pair<int, int>> q;
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (field[i][j] == 1 && !visited[i][j]) {
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                if (field[i][j] == 1 && !visited[i][j]) 
+                {
                     count++;
                     q.push({i, j});
                     visited[i][j] = true;
 
-                    while (!q.empty()) {
+                    while (!q.empty()) 
+                    {
                         int cx = q.front().first;
                         int cy = q.front().second;
                         q.pop();
 
-                        for (int d = 0; d < 4; d++) {
+                        for (int d = 0; d < 4; d++) 
+                        {
                             int nx = cx + dx[d];
                             int ny = cy + dy[d];
 
-                            if (nx >= 0 && ny >= 0 && nx < m && ny < n && field[nx][ny] == 1 && !visited[nx][ny]) {
+                            if (nx >= 0 && ny >= 0 && nx < m && ny < n && field[nx][ny] == 1 && !visited[nx][ny]) 
+                            {
                                 visited[nx][ny] = true;
                                 q.push({nx, ny});
                             }
