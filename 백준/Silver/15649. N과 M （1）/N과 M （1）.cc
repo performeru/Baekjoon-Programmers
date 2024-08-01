735cdbd043a8520eab389d0c6a9da4f091c9ff67
performeru <130340605+performeru@users.noreply.github.com>
2024-08-01 21:25:55 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[100];
+bool isused[10];
+
+void func(int k)
+{
+    if(k == m)
+    {
+        for(int i = 0; i < m; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << "\n";
+        return;
+    }
+    
+    for(int i = 1; i <= n; i++)
+    {
+        if(!isused[i])
+        {
+            arr[k] = i;
+            isused[i] = 1;
+            func(k + 1);
+            isused[i] = 0;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n >> m;
+    func(0);
+    
+}
