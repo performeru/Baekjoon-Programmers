27a13cd33b5c9b79457cd18d2dd6bd15ab497f60
performeru <130340605+performeru@users.noreply.github.com>
2024-09-15 17:08:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.88 ms, Memory: 6.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> st;
+    
+    for(auto ch : s)
+    {
+        if(!st.empty() && st.top() == ch)
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(ch);
+        } 
+    }
+    
+    return st.empty() ? 1 : 0;
+}
