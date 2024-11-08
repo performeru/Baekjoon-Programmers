08c9ef41f2bd91440a50fad6694a848d6c480662
performeru <130340605+performeru@users.noreply.github.com>
2024-11-08 22:40:37 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    priority_queue<int> pq;  
+    
+    for (int i = 0; i < n; i++) 
+    {
+        int x;
+        cin >> x;
+        
+        if (x == 0)
+        {  
+            if (pq.empty()) 
+            {
+                cout << 0 << '\n';  
+            } 
+            else 
+            {
+                cout << pq.top() << '\n';  
+                pq.pop();  
+            }
+        } 
+        else 
+        {
+            pq.push(x);  
+        }
+    }
+    
+}
