f49c191f8badd50135eadedf2bf350ade584fb3a
performeru <130340605+performeru@users.noreply.github.com>
2024-12-28 21:51:26 +0900

    [Silver V] Title: 나이순 정렬, Time: 40 ms, Memory: 7900 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    vector<pair<int, string>> a(n);
+    
+    // 입력
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> a[i].first >> a[i].second; 
+    }
+    
+    stable_sort(a.begin(), a.end(), [](const pair<int, string> &x, const pair<int, string> &y) 
+    {
+        return x.first < y.first; 
+    });
+    
+    for (const auto &p : a) 
+    {
+        cout << p.first << " " << p.second << "\n";
+    }
+    
+}
