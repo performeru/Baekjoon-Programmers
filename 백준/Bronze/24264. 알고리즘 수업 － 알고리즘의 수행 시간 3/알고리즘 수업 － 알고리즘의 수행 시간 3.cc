33d4cec8a6d98d309c696fe30778e7bd6de47f12
performeru <130340605+performeru@users.noreply.github.com>
2025-08-31 22:40:48 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long n;
+    cin >> n;
+    
+    cout << n * n << '\n';
+    cout << 2 << '\n';
+}
